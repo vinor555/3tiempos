61eb06c038942fbdcdc733aa7832f670f741bcba
vinor555 <kevincoma555@gmail.com>
2021-04-01 09:36:33 -0600

    agregando toast a screen registro

--- app/components/Account/RegisterForm.js
+++ app/components/Account/RegisterForm.js
@@ -4,7 +4,8 @@ import { Input, Icon, Button } from "react-native-elements";
 import { validateEmail } from "../../utils/validations";
 import { size, isEmpty } from "lodash";
 
-export default function RegisterForm() {
+export default function RegisterForm(props) {
+    const { toastRef } = props;
     const [showPassword, setShowPassword] = useState(false);
     const [showRepeatPassword, setShowRepeatPassword] = useState(false)
     const [formData, setFormData] = useState(defaultFormValue)
@@ -15,15 +16,15 @@ export default function RegisterForm() {
             isEmpty(formData.password) || 
             isEmpty(formData.repeatPassword)
         ) {
-            console.log("Todos los campos son obligatorios");
+            toastRef.current.show("Todos los campos son obligatorios");
         } else if (!validateEmail(formData.email)) {
-            console.log("El email no es correcto");
+            toastRef.current.show("El email no es correcto");
         } else if (formData.password !== formData.repeatPassword) {
-            console.log("Las contraseñas tienen que ser iguales");
+            toastRef.current.show("Las contraseñas tienen que ser iguales");
         } else if (size(formData.password) < 6) {
-            console.log("La contraseña debe tener al menos 6 caracteres");
+            toastRef.current.show("La contraseña debe tener al menos 6 caracteres");
         } else {
-            console.log("Todo Correcto y yo que me alegro!!!");
+            toastRef.current.show("Todo Correcto y yo que me alegro!!!");
         }
     }
 
--- app/screens/Account/Register.js
+++ app/screens/Account/Register.js
@@ -1,9 +1,13 @@
-import React from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import React, { useRef } from "react";
+import { StyleSheet, View, Image } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import Toast from "react-native-easy-toast";
 import RegisterForm from "../../components/Account/RegisterForm";
 
+
 export default function Register() {
+    const toastRef = useRef();
+    console.log(toastRef);
     return (
         <KeyboardAwareScrollView>
             <Image
@@ -12,8 +16,9 @@ export default function Register() {
                 style={styles.logo}
             />
             <View style={styles.viewForm}>
-                <RegisterForm />
+                <RegisterForm toastRef={toastRef} />
             </View>
+            <Toast ref={toastRef} position="center" opacity={0.9} />
         </KeyboardAwareScrollView>
     );
 }
