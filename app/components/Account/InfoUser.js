adc6b3053da7d7dbf5edb1186a2ff94479c2147c
vinor555 <kevincoma555@gmail.com>
2021-04-01 21:04:01 -0600

    estructura componente infoUser

---
+++ app/components/Account/InfoUser.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native';
+import { Avatar } from "react-native-elements";
+
+export default function InfoUser(props) {
+    const { userInfo } = props;
+    console.log(userInfo);
+    return (
+        <View style={styles.viewUserInfo}>
+            <Avatar 
+                rounded
+                size="large"
+                showEditButton
+                containerStyle={styles.userInfoAvatar}
+            />
+            <View>
+                <Text style={styles.displayName}>
+                    Kevin Ariel Cruz Ortiz
+                </Text>
+                <Text>
+                    kevincoma555@gmail.com
+                </Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    viewUserInfo: {
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "row",
+        backgroundColor: "#f2f2f2",
+        paddingTop: 30,
+        paddingBottom: 30
+    },
+    userInfoAvatar: {
+        marginRight: 20
+    },
+    displayName: {
+        fontWeight: "bold",
+        paddingBottom: 5
+    }
+});
--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -1,18 +1,27 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { Button } from "react-native-elements";
 import Toast from "react-native-easy-toast";
 import * as firebase from "firebase";
 import Loading from "../../components/Loading";
+import InfoUser from "../../components/Account/InfoUser";
 
 export default function UserLogged() {
+    const [userInfo, setUserInfo] = useState(null)
     const [loading, setLoading] = useState(false);
     const [loadingText, setLoadingText] = useState("");
     const toastRef = useRef();
 
+    useEffect(() => {
+        (async () => {
+            const user = await firebase.auth().currentUser;
+            setUserInfo(user);
+        })()
+    }, [])
+
     return (
         <View style={styles.viewUserInfo}>
-            <Text>InfoUser...</Text>
+            {userInfo && <InfoUser userInfo={userInfo} />}
             <Text>AccountOptions...</Text>
             <Button 
                 title="Cerrar Sesión" 
