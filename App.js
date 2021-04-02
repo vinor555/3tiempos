c037e9d2d0b38e3aa20c2e248e77ab32eca9796a
vinor555 <kevincoma555@gmail.com>
2021-04-01 22:26:06 -0600

    actualizando avatar en usuario firebase

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { YellowBox } from "react-native";
+import { firebaseApp } from "./app/utils/firebase";
 import Navigation from "./app/navigations/Navigation";
-import { firebaseApp } from "./app/utils/firebase"
+
+YellowBox.ignoreWarnings(["Setting a timer"]);
 
 export default function App() {
   return <Navigation />;
