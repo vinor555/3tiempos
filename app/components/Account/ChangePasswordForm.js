3e1edc9ed723dcbf0a0f37b2308c4a8839c60fa4
vinor555 <kevincoma555@gmail.com>
2021-11-10 04:10:15 -0600

    agregando nuevos restaurantes

--- app/components/Account/ChangePasswordForm.js
+++ app/components/Account/ChangePasswordForm.js
@@ -1,18 +1,66 @@
 import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { Input, Button } from "react-native-elements";
+import { size } from "lodash";
+import * as firebase from "firebase";
+import { reauthenticate } from "../../utils/api";
 
-export default function ChangePasswordForm() {
+export default function ChangePasswordForm(props) {
+    const { setShowModal, toastRef } = props;
     const [showPassword, setShowPassword] = useState(false);
     const [formData, setFormData] = useState(defaultValue());
+    const [errors, setErrors] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
 
     const onChange = (e, type) => {
         setFormData( {...formData, [type]: e.nativeEvent.text});
     };
 
-    const onSubmit = () => {
-        console.log(formData);
-    }
+    const onSubmit = async () => {
+        let isSetErrors = true;
+        let errorsTemp = {};
+        setErrors({});
+        if(!formData.password || !formData.newPassword || !formData.repeatNewPassword) {
+            errorsTemp = {
+                password: !formData.password ? "La contraseña no puede estar vacia." : "",
+                newPassword: !formData.newPassword ? "La contraseña no puede estar vacia." : "",
+                repeatNewPassword: !formData.repeatNewPassword ? "La contraseña no puede estar vacia." : ""
+            };
+        } else if (formData.newPassword !== formData.repeatNewPassword) {
+            errorsTemp = {
+                newPassword: "Las contraseñas no son iguales.",
+                repeatNewPassword: "Las contraseñas no son iguales."
+            };
+        } else if (size(formData.newPassword) < 6) {
+            errorsTemp = {
+                newPassword: "La contraseña tiene que ser mayor a 5 caracteres.",
+                repeatNewPassword: "La contraseña tiene que ser mayor a 5 caracteres."
+            };
+        } else {
+            setIsLoading(true);
+            await reauthenticate(formData.password).then(async () => { //colocando el async en la funcion hacemos que con el await espera hasta que retorne la promesa y no ejecute el setErrors antes y mande vacio. :OOO
+                await firebase.auth().currentUser.updatePassword(formData.newPassword).then(() => {
+                    isSetErrors = false;
+                    setIsLoading(false);
+                    setShowModal(false);
+                    firebase.auth().signOut();
+                }).catch(() => {
+                    errorsTemp = {
+                        other: "Error al actualizar la contraseña."
+                    }
+                    setIsLoading(false);
+                });
+            }).catch(() => {
+                errorsTemp = {
+                    password: "La contraseña no es correcta.",
+                };
+                setIsLoading(false);
+            });
+        }
+        isSetErrors && setErrors(errorsTemp);
+    };
+
+    
 
     return (
         <View style={styles.view}>
@@ -28,6 +76,7 @@ export default function ChangePasswordForm() {
                     onPress: () => setShowPassword(!showPassword)
                 }}
                 onChange={e => onChange(e, "password")}
+                errorMessage={errors.password}
             />
             <Input
                 placeholder="Nueva contraseña"
@@ -41,6 +90,7 @@ export default function ChangePasswordForm() {
                     onPress: () => setShowPassword(!showPassword)
                 }}
                 onChange={e => onChange(e, "newPassword")}
+                errorMessage={errors.newPassword}
             />
             <Input
                 placeholder=" Repetir Nueva contraseña"
@@ -54,13 +104,16 @@ export default function ChangePasswordForm() {
                     onPress: () => setShowPassword(!showPassword)
                 }}
                 onChange={e => onChange(e, "repeatNewPassword")}
+                errorMessage={errors.repeatNewPassword}
             />
             <Button
                 title="Cambiar contraseña"
                 containerStyle={styles.btnContainer}
                 buttonStyle={styles.btn}
                 onPress={onSubmit}
+                loading={isLoading}
             />
+            <Text>{errors.other}</Text>
         </View>
     );
 }
--- app/navigations/RestaurantsStack.js
+++ app/navigations/RestaurantsStack.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import Restaurants from "../screens/Restaurants";
+import Restaurants from "../screens/Restaurants/Restaurants";
+import AddRestaurant from "../screens/Restaurants/AddRestaurant";
 
 const Stack = createStackNavigator();
 
 export default function RestaurantsStack() {
-    return(
-        <Stack.Navigator>
-            <Stack.Screen
-                name="restaurants"
-                component={Restaurants}
-                options={{ title: "Restaurantes"}}
-            />
-        </Stack.Navigator>
-    )
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="restaurants"
+        component={Restaurants}
+        options={{ title: "Restaurantes" }}
+      />
+      <Stack.Screen
+        name="add-restaurant"
+        component={AddRestaurant}
+        options={{ title: "Añadir nuevo restaurante" }}
+      />
+    </Stack.Navigator>
+  );
 }
